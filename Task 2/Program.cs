7a0202763ef7789eeedf886eb638afbf054c81ad
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-17 00:29:22 +0500

    Broke Task 2 but fixed

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,11 @@
 ﻿Console.WriteLine("Введите трёхзначное целое число");
-num i = Console.ReadLine();
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 100 / 10;
+if (a > 99 && a < 999) // решил сделать просто без конверсии и парсинга строки пока
+{
+Console.WriteLine($"вторая цифра в числе: {b}");
+}
+else
+{
+Console.WriteLine("Вы ввели неверное число");    
+}
