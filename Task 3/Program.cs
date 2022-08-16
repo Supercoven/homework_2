fe573c26b2da033e5d8ca22e23b29ef4324613e5
Burundukov Denis <burundukov.d@yandex.ru>
2022-08-17 00:57:33 +0500

    Task 3 completed

---
+++ Task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 5 && a < 8) 
+{
+Console.WriteLine("Ура, сегодня выходной!");
+}
+else if (a > 0 && a < 6)
+{
+Console.WriteLine("Вставай, иди на работу!");    
+}
+else
+{
+Console.WriteLine("Хорошая пятница вчера была, да?"); 
+}
