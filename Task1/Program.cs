be13338c7542379c5f575b9ebd818b501596d844
Burundukov Denis <burundukov.d@yandex.ru>
2022-11-03 21:16:28 +0500

    Task 1 completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,27 +1,9 @@
-﻿// void TriNumber()
-// {
-// int b = a % 100 / 10;
-// Console.WriteLine($"вторая цифра в числе: {b}");
-// }
-
-// Console.WriteLine("Введите трёхзначное целое число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int b = 0;
-// TriNumber b;
-Console.WriteLine("Введите трёхзначное число");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = 0;
-
-void Tri()
+﻿void Tri(int a)
 {
-    if (a >99 & a < 1000)
-    {
-        Console.WriteLine("Число не трёхначное");
-        return;
-    }
-    else (int b = a % 100 / 10);
-    {
+int b = a % 100 / 10;
 Console.WriteLine($"вторая цифра в числе: {b}");
-    }
+}
 
-Tri(int a);
+Console.WriteLine("Введите трёхзначное целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Tri(a);
