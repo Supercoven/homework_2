266a4c3f2cfbaf966d9fc722b715b3645a06bc76
Burundukov Denis <burundukov.d@yandex.ru>
2022-11-06 14:54:37 +0500

    Task 2 completed

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,11 +1,17 @@
-﻿Console.WriteLine("Введите трёхзначное целое число");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = a % 100 / 10;
-if (a > 99 && a < 999)
-
-Console.WriteLine($"вторая цифра в числе: {b}");
-}
-else
+﻿internal class Program
 {
-Console.WriteLine("Вы ввели неверное число");    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое трёхзначное число");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        void Third()
+        {
+            int b = a % 100 / 10;
+            if (a > 99 & a < 999) Console.WriteLine($"вторая цифра в числе: {b}");
+            else Console.WriteLine("Вы ввели неверное число");
+        }
+
+        Third();
+    }
 }
